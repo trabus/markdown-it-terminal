0a79928fae07f01a1537d2c84e0d143c19ee279e
Jake Bixby <trabus@gmail.com>
2015-03-09 09:44:12 -0700

    More tests

--- tests/test.js
+++ tests/test.js
@@ -34,10 +34,20 @@ describe('markdown-it-terminal', function () {
     expect(md.render('# h1\n## h2\n### h3\n#### h4\n##### h5\n###### h6'))
       .to.equal('\u001b[35m\u001b[4m\u001b[1m\nh1\u001b[22m\u001b[24m\u001b[39m\n\u001b[32m\u001b[1m\nh2\u001b[22m\u001b[39m\n\u001b[32m\u001b[1m\nh3\u001b[22m\u001b[39m\n\u001b[32m\u001b[1m\nh4\u001b[22m\u001b[39m\n\u001b[32m\u001b[1m\nh5\u001b[22m\u001b[39m\n\u001b[32m\u001b[1m\nh6\u001b[22m\u001b[39m\n');
   });
-
+//TODO: nested list test
   it('renders ordered list', function() {
     expect(md.render('1. Item 1\n2. Item 2\n3. Item 3'))
       .to.equal('   \u001b[0m1 Item 1\u001b[0m\n   \u001b[0m2 Item 2\u001b[0m\n   \u001b[0m3 Item 3\u001b[0m\n\u001b[0m\n');
+  });
+  
+  it('renders strong', function(){
+    expect(md.render('**strong**')).to.equal('\u001b[0m\u001b[1mstrong\u001b[22m\u001b[0m\n\n');
+    expect(md.render('__strong__')).to.equal('\u001b[0m\u001b[1mstrong\u001b[22m\u001b[0m\n\n');
+  });
+  
+  it('renders em', function(){
+    expect(md.render('*em*')).to.equal('\u001b[0m\u001b[3mem\u001b[23m\u001b[0m\n\n');
+    expect(md.render('_em_')).to.equal('\u001b[0m\u001b[3mem\u001b[23m\u001b[0m\n\n');
   });
   
   it('renders s (del)', function(){
