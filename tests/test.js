129a23df497fcc37bed5537ec9691a27c7844ee1
Jake Bixby <trabus@gmail.com>
2015-03-12 14:23:24 -0700

    Initial pass at color blocks and inline (not enabled)

--- tests/test.js
+++ tests/test.js
@@ -105,4 +105,10 @@ describe('markdown-it-terminal', function () {
       'bar\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m \u001b[33m{\u001b[39m\n   \u001b[31m'+
       'return\u001b[39m \u001b[92m"bar"\u001b[39m\u001b[90m;\u001b[39m\n\u001b[33m}\u001b[39m\n\u001b[39m\n\n');
   });
+  
+  it('renders blue', function(){   
+    console.log(md.render('<blue>content is blue and <red>this should be red</red> but this is blue</blue>'))  
+    expect(md.render('<blue>blue</blue>'))
+      .to.equal('\u001b[0m\u001b[2m\u001b[90m\u001b[9mstrike\u001b[29m\u001b[39m\u001b[22m\u001b[0m\n\n');
+  });
 });
